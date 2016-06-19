a84aeec9b6069052e41989d355c511566d0124ee
Tim Heithecker <tim.heithecker@gmail.com>
2016-06-19 23:53:58 +0200

    tmp

--- Booksprint_ext.hooks.php
+++ Booksprint_ext.hooks.php
@@ -18,40 +18,79 @@ class Booksprint_extHooks {
 	public static function onParserSetup(Parser $parser){
 		$parser->setHook("bookinfo", "Booksprint_extHooks::renderTagBookinfo");
 	}
+
+	private static function infoRow($name, $val) {
+		return '<tr><td><b>' . $name . '</b></td><td>' . $val . '</td><tr>';
+	}
 	public static function renderTagBookinfo($input, array $args, Parser $parser, PPFrame $frame){
 		$html = "";
+		global $wgOut;
 		$title = $parser->getTitle();
-		$required = array( "abstract", "status", "info", "authors");
-		$missing = array();
+		$required = array( "abstract", "authors");
+		// allowed allso defines the order
+		$allowed = array_merge($required, array("info", "contributors"));
+		//die(var_dump($allowed));
 		$argsKeys = array_keys($args);
+		if($input != ""){
+			$args['abstract'] = $input;
+		}
+		//die(var_dump($args));
+		$vals = array();
+		foreach($args as $k=>$v){
+			if(! in_array($k, $allowed)){
+				$html .=  '<b class="error">Unbekannter Parameter: '. $k. '  </b><br>';
+				continue;
+			}
+			//$html .=  "<br>RE NOW" . implode(", ", $required) . "<br>";
+			//
+			if(substr($v, 0, 3) !== "{{{"){
+				if (in_array($k, $required)){
+					unset($required[array_search($k, $required)]);
+					//die(var_dump($required));
+				}
+				$vals[$k] = htmlspecialchars($v);
+			}
+			
+		}
+
 		foreach ($required as $r){
-			if (! in_array($r , $argsKeys)){
-				$missing[] = '<b class="error">Angabe fehlt: '. $r. '  </b>';
-			}
-
+			$html .= '<b class="error">Angabe fehlt: '. $r. 
+				'.Bitte die Angaben korrigieren.</b>';
 		} 
-		if (sizeof($missing)){
-			$html .= implode($missing, "<br>");
-		} //else {
 
-			foreach($args as $key=>$value){
-				$html .= "<h2>K $key : V $value";
+		//die(var_dump($allowed));
+
+		$html .= "<table>";	 
+		$html .= '<tr><td colspan="2">' .  $vals[array_shift($allowed)] . '</tr></td>'; // abstract
+		foreach($allowed as $key){
+			if (!isset($vals[$key])){
+				continue;
 			}
-		//}
-		//die("HERE");
+			
+			$html .= Booksprint_extHooks::infoRow($wgOut->msg($key), $vals[$key]);
+		}
+		$html .= "</table>";	 
+
 		$anyFile = false;
 		$fileTypes = array("pdf", "odt", "epub");
+		$filelinksHtml = "";
 		foreach($fileTypes as $type){
 
 			$name = "${title}_live.${type}";
 			$f = wfFindFile($name);
-			$html .= "<hr>$name: ";
 			if ($f){
-				$html .= " exists";
+				$filelinksHtml .= '<a class="' . $type . '-link" href="' . $f->getCanonicalUrl() . '">' .
+					$name . '</a> ';
 			}else {
-				$html .= " NOT exists";
+			//	$html .= "$name missing";
 			}
 		}		
+		if($filelinksHtml != ""){
+			$html .= '<div class="bookfilelinks">' . $filelinksHtml . '</a>';
+		}
+
+		//versions
+
 		return $html;
 	}
 }
